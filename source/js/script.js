372a9e8025a39bbf7482d0908700d6090d7345d4
Taron Mesropyan <krastiart@gmail.com>
2023-05-28 19:10:19 +0300

    finish

--- source/js/script.js
+++ source/js/script.js
@@ -25,3 +25,46 @@ function navToggle() {
     }
   }
 }
+
+// Яндекс карта
+let mapPinSrc = '../img/map-pin/map-pin-mobile@1x.png';
+let mapPinSize = [57, 53];
+let dpr = window.devicePixelRatio;
+
+if (document.documentElement.clientWidth >= 768) {
+  mapPinSrc = '../img/map-pin/map-pin-tablet@1x.png';
+  mapPinSize = [113, 106];
+  if (dpr >= 2) {
+    mapPinSrc = '../img/map-pin/map-pin-tablet@2x.png';
+  }
+}
+else {
+  if (dpr >= 2) {
+    mapPinSrc = '../img/map-pin/map-pin-mobile@2x.png';
+  }
+}
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init(){
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [59.938631, 30.323037],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 15
+  });
+  // Метка на карте
+  var myPlacemark = new ymaps.Placemark([59.938631, 30.323037], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: mapPinSrc,
+    iconImageSize: mapPinSize,
+    iconImageOffset: [-25, -53]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
--- source/js/slider.js
+++ source/js/slider.js
@@ -5,11 +5,9 @@ let sliderBefore = document.querySelector(".slider__before");
 let sliderAfter = document.querySelector(".slider__after");
 
 // слайдер
-rangeButton.addEventListener( "mousedown", (slider) );
-rangeButton.addEventListener( "touchstart", (slider) );
+rangeButton.addEventListener( "input", (slider) );
 
 function slider() {
-  window.requestAnimationFrame(slider);
   sliderBefore.style.width = rangeButton.value + "%";
   sliderAfter.style.width = (100 - rangeButton.value) + "%";
   curtain.style.left = `calc(${rangeButton.value}% - ${20}px)`;
